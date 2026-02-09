5abbf2e452c222bb5acb0cf4ef8d89209706de96
gustavoHermilo <0gustavogarciagarcia@gmail.com>
2026-02-08 18:03:05 -0600

    "Add Plant class and protection system"

--- main.py
+++ main.py
@@ -1,57 +1,78 @@
 import random
-# Plant Simulator - Version 0.4
-
+# Plant Simulator - Version 0.5
+class Plant:
+    def __init__(self):
+        self.health=100
+        self.water=50
+        self.nutrients=50
+        self.protected=False  #greenhouse / cover active
+    def status(self):
+        print("\n Current Plant Status")
+        print("Health:",self.health)
+        print("Water:",self.water)
+        print("Nutrients:",self.nutrients)
+        print("Protected:",self.protected)
+plant=Plant()
 day= 1
-health= 100
-water = 50
-nutrients= 50
 
-while health>0:
+while plant.health>0:
     print("\n-----------------------------------")
     print("Day",day)
 
     weather= random.choice(["sun","snow","hail"])
     print("Weather today:",weather)
 
+    #Weather effects
     if weather== "sun":
-        water -= 10
+        plant.water -= 10
         print("The sun dries the soil.Water -10")
     elif weather == "snow":
-        health -= 15
-        print("Snow damages the plant! Health -15")
+        if plant.protected:
+            plant.health -= 5
+            print("Snow is blocked by protection.Health -5")
+        else:
+            plant.health-= 5
+            print("Snow damages the plant. Health -15")
     elif weather == "hail":
-        health -= 25
-        print("Hail is very harmful! Health -25")
+        if plant.protected:
+            plant.health-=10
+            print("Hail blocked by protection.Health -10")
+        else:
+            plant.health -= 25
+            print("Hail is very harmful! Health -25")
 
-    if water<0:
-        water=0
-    if nutrients<0:
-        nutrients=0
-    if health<0:
-        health=0
-
-    print("\n Current Plant Status")
-    print("Health:",health)
-    print("Water:",water)
-    print("Nutrients:",nutrients)
+    if plant.water<0:
+        plant.water=0
+    if plant.nutrients<0:
+        plant.nutrients=0
+    if plant.health<0:
+        plant.health=0
+    plant.status()
 
     print("\nWhat do you want to do?")
-    print("1.Water the plant (+20 water)")
+    print("1.Water plant (+20 water)")
     print("2.Add nutrients (+15 nutrients)")
-    print("Do nothing")
+    print("3.Activate protection(greehouse)")
+    print("4.Do nothing")
 
-    choice=input("Choose an option (1/2/3):")
+    choice=input("Choose an option (1/2/3/4):")
 
     if choice=="1":
-        water+=20
-        print("You water the plant.Water +20")
+        plant.water+=20
+        print("You watered the plant.Water +20")
     elif choice=="2":
-        nutrients+=15
+        plant.nutrients+=15
         print("You added nutrients.Nutrients +15")
     elif choice=="3":
+        plant.protected=True
+        print("Protection activated.")
+    elif choice=="4":
         print("You did nothing today.")
     else: 
-        print("Invalid choice.Nothing happened")
+        print("Invalid choice.")
+
+    #Protection Lasts one day only 
+    plant.protected=False 
     
     day+=1
 print("\n The plant has died")
